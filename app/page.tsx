d5929906501aebe9238628507b8cea26361be423
andja <andja.velimirovic@gmail.com>
2023-04-02 12:57:06 +0200

    render table with products

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
-export default function Home() {
+"use client";
+import { products } from "@/products";
+
+function Home() {
   return (
-    <main>
-      <div>Products</div>
+    <main className="flex items-center justify-center w-full p-8">
+      <table className="table-auto border-collapse border border-slate-400 border-spacing-2 w-4/5 py-8 text-sm">
+        <thead>
+          <tr className="border-collapse border border-slate-400 p-2">
+            <th className="border-collapse border border-slate-400 p-2">
+              Title
+            </th>
+            <th className="p-2" colSpan={2}>
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr
+              key={product.id}
+              className="border-collapse border border-slate-400 p-2 align-middle"
+            >
+              <td className="border-collapse border border-slate-400  py-1 px-3">
+                {product.title}
+              </td>
+              <td className="border-collapse border border-slate-400 p-2">
+                <button className="bg-green-500 text-white py-1 px-3">
+                  Edit
+                </button>
+              </td>
+              <td className="border-collapse border border-slate-400 p-2">
+                <button className="bg-red-500 text-white py-1 px-3">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
+
+export default Home;
---
+++ products.ts
@@ -0,0 +1,146 @@
+import product1 from "./public/images/product1.jpg";
+import product2 from "./public/images/product2.jpg";
+import product3 from "./public/images/product3.jpg";
+import product4 from "./public/images/product4.jpg";
+import product5 from "./public/images/product5.jpg";
+import product6 from "./public/images/product6.jpg";
+import product7 from "./public/images/product7.jpg";
+import product8 from "./public/images/product8.jpg";
+import product9 from "./public/images/product9.jpg";
+import product10 from "./public/images/product10.jpg";
+import product11 from "./public/images/product11.jpg";
+import product12 from "./public/images/product12.jpg";
+import product13 from "./public/images/product13.jpg";
+import product14 from "./public/images/product14.jpg";
+import product15 from "./public/images/product15.jpg";
+
+export interface IProduct  {
+  id: number,
+  title: string, 
+  info: string,
+  description: string, 
+  img: any
+}
+
+export const products : IProduct[] = [
+  {
+    id: 1,
+    title: "Anchor bracelet",
+    info: "Anchor bracelet gold for women.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product1,
+  },
+  {
+    id: 2,
+    title: "Bangle bracelet",
+    info: "Women bracelet with feathers",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product2,
+  },
+  {
+    id: 3,
+    title: "Bird necklace",
+    info: "Gold necklace with bird",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product3,
+  },
+  {
+    id: 4,
+    title: "Buddha bracelet",
+    info: "Buddha bracelet for men",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product4,
+  },
+  {
+    id: 5,
+    title: "Embroidered bracelet",
+    info: "Embroidered bracelet for women",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product5,
+  },
+  {
+    id: 6,
+    title: "Arrow bracelet",
+    info: "Gold arrow bracelet for women",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product6,
+  },
+  {
+    id: 7,
+    title: "Patterned bracelet",
+    info: "Patterned bangle bracelet for women",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product7,
+  },
+  {
+    id: 8,
+    title: "Gold necklace",
+    info: "Gold necklace with details",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product8,
+  },
+  {
+    id: 9,
+    title: "Simple bracelet",
+    info: "Simple gold bracelet for women",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product9,
+  },
+  {
+    id: 10,
+    title: "Stacked bracelet",
+    info: "Stacked gold bracelet for women",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product10,
+  },
+  {
+    id: 11,
+    title: "Anchor mens bracelet",
+    info: "Anchor bracelet gold for men.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product11,
+  },
+  {
+    id: 12,
+    title: "Boho earrings",
+    info: "Boho earrings gold for women.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product12,
+  },
+  {
+    id: 13,
+    title: "Nautical bracelet",
+    info: "Nautical charm bracelet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product13,
+  },
+  {
+    id: 14,
+    title: "Boho bracelet",
+    info: "Boho bangle bracelet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product14,
+  },
+  {
+    id: 15,
+    title: "Bracelets set",
+    info: "Stacked gold bracelets set.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptates at deleniti iure. Tenetur, cum minima dolores pariatur maxime, consequuntur ipsam necessitatibus molestiae, aperiam facere perferendis iste! Velit, unde ex.",
+    img: product15,
+  },
+];
