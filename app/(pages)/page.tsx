a4df9b99dbe9be63f210b878e5e900ed596868c8
andja <andja.velimirovic@gmail.com>
2023-04-04 14:22:03 +0200

    add table margin bottom

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -4,17 +4,15 @@ import Link from "next/link";
 
 function Home() {
   return (
-    <main className="flex flex-col justify-center align-center text-primary max-w-4xl mx-auto px-2">
+    <main className="flex flex-col justify-center align-center text-primary max-w-4xl mx-auto px-2 mb-10">
       <header className="flex justify-between align-center my-5">
         <h2 className="font-title font-extrabold text-lg">Products</h2>
-
         <Link href="/create" className="text-sm text-white font-medium">
           <button className="bg-positive font-medium py-2 px-6">
             Create New Product
           </button>
         </Link>
       </header>
-
       <table className="table-auto py-8 text-sm">
         <thead>
           <tr>
