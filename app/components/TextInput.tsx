2a74333734a4a76fa930fe21908413237b072a25
andja <andja.velimirovic@gmail.com>
2023-05-03 18:36:57 +0200

    apply classname to text input

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -16,8 +16,7 @@ export default function TextInput({ value, name, handleChange }: Props) {
         value={value}
         type="text"
         name={name}
-        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+        className="form-text"
         onChange={handleChange}
       />
     </label>
