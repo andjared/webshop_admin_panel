b3657d1d9151e81d83d79194a6bc8378a43343ae
andja <andja.velimirovic@gmail.com>
2023-05-03 16:48:53 +0200

    fix productRow links

--- app/components/ProductRow.tsx
+++ app/components/ProductRow.tsx
@@ -17,18 +17,18 @@ export default function ProductRow({ id, title, handleDelete }: Props) {
       <td className="table-data py-1 px-3 font-medium">{title}</td>
       <td className="table-data p-2">
         <Link href={`/edit/${id}`} className="text-sm z-10">
-          <span className="bg-positive text-white font-medium py-2 px-6 pointer-events-auto">
+          <span className="inline-block bg-positive text-white font-medium py-2 px-6 pointer-events-auto">
             Edit
           </span>
         </Link>
       </td>
-      <td className="table-data p-2">
-        <button
-          className="bg-warning text-white font-medium py-2 px-6"
+      <td className="table-data p-2 pointer-events-auto cursor-pointer">
+        <span
+          className="inline-block bg-warning text-white font-medium py-2 px-6"
           onClick={deleteProduct}
         >
           Delete
-        </button>
+        </span>
       </td>
     </tr>
   );
